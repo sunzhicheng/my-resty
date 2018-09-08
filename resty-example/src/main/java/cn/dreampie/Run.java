82b6774e33307da9db93d2268eb983bebdcdae12
walker <sun1020@163.com>
2018-09-08 14:19:56 +0800

    运行第一版

--- resty-example/src/main/java/cn/dreampie/Run.java
+++ resty-example/src/main/java/cn/dreampie/Run.java
@@ -5,7 +5,7 @@ import cn.dreampie.server.provider.jetty.JettyServerProvider;
 /**
  * Created by Dreampie on 16/9/6.
  */
-public class ExampleApplication {
+public class Run {
 
   public static void main(String[] args) throws Exception {
     new JettyServerProvider().build().start();
